730f0fe8e044adaecbadc46de8e12fd1e77710e0
Dhathri Rudroju <dhathrirudroju@gmail.com>
2026-04-18 07:33:22 +0530

    Update drugRoutes.js

--- Pharma-ai-backend/routes/drugRoutes.js
+++ Pharma-ai-backend/routes/drugRoutes.js
@@ -3,21 +3,10 @@ const router = express.Router();
 
 const {
   getAllDrugs,
-  searchDrugs,
-  getDrugByName,
-  getDrugAnalysis,
-  scanDrugText,
-  chatWithAI
+  getDrugAnalysis
 } = require("../controllers/drugController");
 
-// Basic routes
 router.get("/", getAllDrugs);
-router.get("/search", searchDrugs);
-router.get("/:name", getDrugByName);
 router.get("/:name/analysis", getDrugAnalysis);
-
-// Extra features
-router.post("/scan", scanDrugText);
-router.post("/chat", chatWithAI);
 
 module.exports = router;
