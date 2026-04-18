c3649d42855e54887450b808e4a424c8649a02b8
dhathri rudroju <dhathrirudroju@gmail.com>
2026-04-18 07:14:35 +0530

    initial commit

--- Pharma-ai-backend/services/sheetservice.js
+++ Pharma-ai-backend/services/sheetservice.js
@@ -1,16 +1,17 @@
-const axios = require("axios");
+const organs = [
+  "Brain",
+  "Lungs",
+  "Heart",
+  "Liver",
+  "Pancreas",
+  "Kidneys"
+];
 
-const fetchDrugData = async () => {
-  const res = await axios.get(process.env.SHEETY_DRUG_DATA);
-  return res.data.comprehensiveDrugDiseaseDataGeneration || [];
+const mapOrganGlow = (primaryOrgan) => {
+  return organs.map(org => ({
+    name: org,
+    active: org.toLowerCase() === primaryOrgan?.toLowerCase()
+  }));
 };
 
-const fetchEffectsData = async () => {
-  const res = await axios.get(process.env.SHEETY_EFFECTS_DATA);
-  return res.data.drugBenefitsAndSideEffectsDatabase || [];
-};
-
-module.exports = {
-  fetchDrugData,
-  fetchEffectsData
-};
+module.exports = { mapOrganGlow };
